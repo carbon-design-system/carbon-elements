9203a6fae07c184adf7cc1d44af94bd3a594b8c5
Josh Black <joshblack@us.ibm.com>
2018-09-17 17:45:41 -0500

    feat(type): add type package

--- __tests__/styles-test.js
+++ __tests__/styles-test.js
@@ -9,21 +9,14 @@ const sass = require('node-sass');
 const glob = require('glob');
 
 const defaultOptions = {
-  includePaths: [
-    'node_modules',
-    path.resolve(__dirname, '../../../node_modules'),
-  ],
+  includePaths: ['node_modules'],
 };
 const cwd = path.resolve(__dirname, '../');
-const files = glob.sync('**/*.scss', {
+const files = glob.sync('packages/**/*.scss', {
   cwd,
 });
 
-describe('colors', () => {
-  it('should have a generated file', () => {
-    expect(files.length > 0).toBe(true);
-  });
-
+describe('styles', () => {
   it.each(files)('%s should compile', (relativeFilePath, done) => {
     const filepath = path.join(cwd, relativeFilePath);
     sass.render(
--- packages/type/src/index.js
+++ packages/type/src/index.js
@@ -1,16 +1,13 @@
 'use strict';
 
-function getTypeSize(step) {
-  if (step === 1) {
-    return 12;
-  }
-  // Xn = Xn-1 + {INT[(n-2)/4] + 1} * 2
-  return getTypeSize(step - 1) + (Math.floor((step - 2) / 4) + 1) * 2;
-}
-
-const scale = Array.from({ length: 17 }, (_, i) => getTypeSize(i + 1));
+const fontFamilies = require('./fontFamilies');
+const { baseFontSize, getTypeSize, scale } = require('./scale');
+const tokens = require('./tokens');
 
 module.exports = {
+  baseFontSize,
+  fontFamilies,
   getTypeSize,
   scale,
+  tokens,
 };
