c988e3905825a9237137a6b8136da154b0914e80
Josh Black <josh@josh.black>
2018-11-12 09:38:26 -0600

    feat(icons-vue): add @carbon/icons-vue package

---
+++ packages/icons-vue/examples/storybook/.storybook/config.js
@@ -0,0 +1,18 @@
+import { addDecorator, configure } from '@storybook/vue';
+// import { withInfo } from '@storybook/addon-info';
+// import { withOptions } from '@storybook/addon-options';
+
+// withOptions({
+// name: `@carbon/icons-react`,
+// url:
+// 'https://github.com/IBM/carbon-elements/tree/master/packages/icons-react',
+// });
+
+// addDecorator(withInfo);
+
+function loadStories() {
+  const req = require.context('../stories', true, /\-story\.js$/);
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ packages/icons-vue/src/build.js
@@ -0,0 +1,5 @@
+'use strict';
+
+async function build() {}
+
+module.exports = build;
