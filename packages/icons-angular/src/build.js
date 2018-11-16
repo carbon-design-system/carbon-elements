17cd3b89636ab5a853edb9f8dad7ef62fa356fb4
Cal Smith <callums@ca.ibm.com>
2018-11-16 17:20:17 -0500

    feat(icons-angular): add examples

--- packages/icons-angular/src/build.js
+++ packages/icons-angular/src/build.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra");
 const { join, resolve } = require("path");
 const { param, lower, pascal } = require("change-case");
-const { componentTemplate, moduleTemplate, indexTemplate } = require("./templates");
+const { componentTemplate, moduleTemplate, indexTemplate, storyTemplate } = require("./templates");
 const ngc = require("@angular/compiler-cli/src/main");
 
 // generate a safe class name
@@ -14,7 +14,6 @@ const formatClassName = (fileName, size) => {
 }
 
 async function generateComponents() {
-  await fs.mkdir("ts");
   const svgPath = resolve(require.resolve("@carbon/icons"), "../../svg");
   const dirs = await fs.readdir(svgPath);
   const exportList = [];
@@ -34,10 +33,12 @@ async function generateComponents() {
         if (!dirExists) {
           await fs.mkdir(join("ts", dirName))
         }
-        await fs.writeFile(join("ts", dirName, `${dir}.ts`), componentTemplate(selectorName, className, rawSvg));
+        await fs.writeFile(join("ts", dirName, `${dir}.ts`), componentTemplate(selectorName, className, rawSvg, dir));
         exportList.push({
           name: className,
-          dir: join(dirName, dir)
+          selector: selectorName,
+          dir: join(dirName, dir),
+          size: dir
         });
       } catch (err) {
         console.error(err);
@@ -50,22 +51,44 @@ async function generateComponents() {
   } catch (err) {
     console.log(err);
   }
+  return exportList;
+}
+
+async function buildExamples(icons) {
+  await fs.copy("lib", "examples/storybook/lib");
+  let filesToWrite = [];
+  for (const icon of icons) {
+    filesToWrite.push(fs.writeFile(`examples/storybook/stories/${icon.selector}.stories.ts`, storyTemplate(icon)));
+  }
+  await Promise.all(filesToWrite);
 }
 
 async function build() {
   console.log("Cleaning build dirs...");
   try {
-    await fs.remove("ts");
-    await fs.remove("lib");
-    await fs.remove("waste");
+    await Promise.all([
+      fs.remove("ts"),
+      fs.remove("lib"),
+      fs.remove("waste"),
+      fs.remove("examples/storybook/lib"),
+      fs.remove("examples/storybook/stories")
+    ]);
+
+    await Promise.all([
+      fs.mkdir("examples/storybook/stories"),
+      fs.mkdir("ts")
+    ]);
   } catch (err) {
     console.error(err);
   }
   console.log("Generating source components...");
-  await generateComponents();
+  const icons = await generateComponents();
   console.log("Compiling and generating modules...");
   // run the angular compiler over everything
   ngc.main(["-p", "./config/tsconfig-aot.json"]);
+  // build the storybook examples
+  console.log("Generating storybook examples...");
+  buildExamples(icons);
 }
 
 module.exports = build;
