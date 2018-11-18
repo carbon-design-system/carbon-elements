c405beb1abfd30ab02292fda58e009d6dc5160b8
Josh Black <josh@josh.black>
2018-11-17 18:17:25 -0600

    fix(icons): update test to exclude dotfiles

--- packages/icons/__tests__/svg-test.js
+++ packages/icons/__tests__/svg-test.js
@@ -8,11 +8,15 @@ const fs = require('fs-extra');
 const klaw = require('klaw-sync');
 const { BUILD_SVG_DIR, SVG_DIR } = require('../src/paths');
 
-const icons = klaw(SVG_DIR, { nodir: true });
-const buildFiles = klaw(BUILD_SVG_DIR, { nodir: true });
+function filter(item) {
+  return item[0] === '.';
+}
+
+const icons = klaw(SVG_DIR, { filter, nodir: true });
+const buildFiles = klaw(BUILD_SVG_DIR, { filter, nodir: true });
 
 describe('svg', () => {
   it('should export all files as optimized svg files', () => {
-    expect(icons.length - 1).toEqual(buildFiles.length);
+    expect(icons.length).toEqual(buildFiles.length);
   });
 });
