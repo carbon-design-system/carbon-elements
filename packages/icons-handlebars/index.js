ea4c932c80c91fa1736ef6114710fad99e2157b4
liz.judd <liz.judd@ibm.com>
2019-01-14 12:25:30 -0500

    feat(icon-handlebars): allow object and hash attributes in same use

--- packages/icons-handlebars/index.js
+++ packages/icons-handlebars/index.js
@@ -15,7 +15,7 @@ const {
 } = require('@carbon/icon-helpers');
 const { SafeString } = require('handlebars');
 
-function iconHelper(name, attributes = {}) {
+function iconHelper(name, attributes = {}, options = {}) {
   const icon = CarbonIcons[name];
   if (!icon) {
     throw new Error(`Unable to find the icon: \`${name}\``);
@@ -23,7 +23,7 @@ function iconHelper(name, attributes = {}) {
   const content = icon.content.map(toString);
   const attrs = {
     ...icon.attrs,
-    ...(attributes.hash || attributes),
+    ...Object.assign({}, attributes.hash || attributes, options.hash),
   };
   return new SafeString(
     `<svg ${formatAttributes(getAttributes(attrs))}>${content.join('')}</svg>`
