7d77315721cfb372e30b6aa2f30489d708b67523
Josh Black <josh@josh.black>
2019-01-18 14:36:11 -0600

    fix(elements): update replacement to use regex

--- packages/elements/tasks/build.js
+++ packages/elements/tasks/build.js
@@ -60,8 +60,14 @@ async function build() {
       );
       await replace({
         files: file.path,
-        from: /\@carbon/g,
-        to: `${relativeImportPath}`,
+        // Regex should match the following package patterns:
+        // @carbon/packagename
+        // @carbon/package-name
+        // Where the package name is the captured group in `match`
+        from: /\@carbon\/([a-z]+)\/scss/g,
+        to(_, match) {
+          return `${relativeImportPath}/${match}`;
+        },
       });
     })
   );
