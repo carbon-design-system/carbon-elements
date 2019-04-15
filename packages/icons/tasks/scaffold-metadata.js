9e1bc248ef8794120405d26e15bfb27337e727dd
Josh Black <josh@josh.black>
2019-04-15 12:06:51 -0500

    chore(icons): update build script to use all sizes for single icon

--- packages/icons/tasks/scaffold-metadata.js
+++ packages/icons/tasks/scaffold-metadata.js
@@ -17,8 +17,11 @@ const SVG_DIR = path.resolve(__dirname, '../src/svg');
 const METADATA_OUTPUT = path.resolve(__dirname, '../metadata.yml');
 
 async function scaffold() {
+  // Get all of our icon files from the SVG directory
   const iconFiles = await search(SVG_DIR);
-  const iconsGroupedByVariant = iconFiles.reduce((acc, icon) => {
+
+  // Group icons by a common basename, collecting all sizes
+  const iconsGroupedByBasename = iconFiles.reduce((acc, icon) => {
     if (acc[icon.basename]) {
       return {
         ...acc,
@@ -30,7 +33,10 @@ async function scaffold() {
       [icon.basename]: [icon],
     };
   }, {});
-  const iconsGroupedByName = Object.keys(iconsGroupedByVariant).reduce(
+
+  // Group icons by common name, this means `add` and `add--filled` are both
+  // grouped under `add` as the name
+  const iconsGroupedByName = Object.keys(iconsGroupedByBasename).reduce(
     (acc, key) => {
       const [name, variant] = key.split('--');
       const group = acc[name] || { icon: null, variants: {} };
@@ -41,13 +47,17 @@ async function scaffold() {
             'This group should not have an icon already: ' + name
           );
         }
-        group.icon = iconsGroupedByVariant[key][0];
+        if (iconsGroupedByBasename[key].length === 0) {
+          group.icon = iconsGroupedByBasename[key][0];
+        } else {
+          group.icons = iconsGroupedByBasename[key];
+        }
       } else {
-        if (!group.variants[key]) {
+        if (!Array.isArray(group.variants[key])) {
           group.variants[key] = [];
         }
 
-        group.variants[key].push(...iconsGroupedByVariant[key]);
+        group.variants[key].push(...iconsGroupedByBasename[key]);
       }
 
       return {
@@ -70,6 +80,8 @@ async function scaffold() {
 
     if (group.icon) {
       icon.sizes = [group.icon.size];
+    } else if (group.icons) {
+      icon.sizes = group.icons.map(icon => icon.size);
     }
 
     if (Object.keys(group.variants).length > 0) {
